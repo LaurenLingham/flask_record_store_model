3c4086af3a31cfc814f0a69ddcd46574e5356f09
LaurenLingham <lauren.lingham@outlook.com>
2023-02-08 17:09:23 +0000

    added order and home page links

--- controllers/controller.py
+++ controllers/controller.py
@@ -4,8 +4,9 @@ from models.order_list import orders
 
 @app.route('/')
 def index():
-    return render_template('index.html', title = "Home Page")
+    return render_template('index.html', title = "Orders", orders = orders)
 
-@app.route('/orders')
-def order_list():
-    return render_template('orders.html', title = "Orders", orders = orders)
+@app.route('/order/<index_num>')
+def single_order(index_num):
+    order = orders[int(index_num)]
+    return render_template('order.html', title = "Customer Order", order = order)
